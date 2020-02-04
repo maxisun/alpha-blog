cb0dafceec9bd4cead039602809199fc298c728a
Max Espinal <00354615@uca.edu.sv>
2020-02-03 18:35:15 -0600

    more data for description

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimun: 3, maximun: 50 }
-  validates :description, presence: true, length: { minimun: 3, maximun: 10_000 }
+  validates :description, presence: true, length: { minimun: 3, maximun: 10_000_000 }
 end
