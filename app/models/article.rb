24a1204725748f97f3179ca028c4d7fcc79467f2
Max Espinal <00354615@uca.edu.sv>
2020-02-07 12:45:26 -0600

    create new articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true, length: { minimun: 3, maximun: 50 }
-  validates :description, presence: true, length: { minimun: 3, maximun: 10_000_000 }
+  validates :title, presence: true, length: { minimum: 3, maximun: 50 }
+  validates :description, presence: true, length: { minimum: 3, maximun: 10_000_000 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
+  # al poner esto, podemos hacer el CRUD con respecto a la tabla articles
+  resources :articles
 end
